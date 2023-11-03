5dc6c69498aae4d68ae041bd2b12b61b35d5d29f
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-11-03 20:38:52 +0100

    field email is created

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,14 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react";
+import isEmail from "validator/lib/isEmail";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-
 type FormData = {
     name: string;
-  
-   
+    email: string;
+
+
 };
 
 const Form = () => {
@@ -15,13 +16,13 @@ const Form = () => {
     const {
         register,
         handleSubmit,
-        formState: { errors },
         reset,
+        formState: { errors },
     } = useForm<FormData>();
 
     const [submittedData, setSubmittedData] = useState<FormData | null>(null);
 
-    const onSubmit: SubmitHandler <FormData> = (data) => {
+    const onSubmit: SubmitHandler<FormData> = (data) => {
         setSubmittedData(data);
         reset();
     };
@@ -42,6 +43,19 @@ const Form = () => {
                     />
                 </div>
 
+                <div className="form-group">
+                    <TextField
+                        fullWidth
+                        label="E-mail"
+                        variant="outlined"
+                        error={!!errors.email}
+                        helperText={errors.email ? "Email is required." : ""}
+                        {...register("email", {
+                            required: true,
+                            validate: (value) => isEmail(value),
+                        })}
+                    />
+                </div>
 
                 <div className="form-group">
                     <Button type="submit" variant="contained" color="primary">
